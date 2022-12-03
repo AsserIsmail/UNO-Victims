27eaf263e238693494d7f843a8f012c5102bc176
Maziar <maziarmehrafza257@gmail.com>
2022-12-02 19:25:23 -0500

    Wrote the CardDecorator class

--- base/ColourblindCards/CardDecorator.java
+++ base/ColourblindCards/CardDecorator.java
@@ -2,5 +2,34 @@ package base.ColourblindCards;
 
 import base.Cards.AbstractCard;
 
+/*
+Card decorator Class
+ */
 public abstract class CardDecorator implements AbstractCard {
+
+    /*
+    Holds the card which will be then decorated
+     */
+    private AbstractCard card;
+
+    /*
+    Get the source of the front image of the card
+     */
+    public String getFrontSrc() {throw new UnsupportedOperationException();}
+
+    /*
+    Set the source of the front image of the card
+     */
+    public void setFrontSrc(String src){throw new UnsupportedOperationException();}
+
+    /*
+    Get the source of the back image of the card
+     */
+    public String getBackSrc(){throw new UnsupportedOperationException();}
+
+    /*
+    Set the source of the back image of the card
+     */
+    public void setBackSrc(String src){throw new UnsupportedOperationException();}
+
 }
