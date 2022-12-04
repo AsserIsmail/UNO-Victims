223196c5841109d79bfdb0e3fc86f3bf23c39cd2
Maziar <maziarmehrafza257@gmail.com>
2022-12-03 19:52:35 -0500

    Added the base code for all decorators

--- base/ColourblindCards/SGBKCard.java
+++ base/ColourblindCards/SGBKCard.java
@@ -1,4 +1,47 @@
 package base.ColourblindCards;
 
+import base.Cards.AbstractCard;
+
 public class SGBKCard extends CardDecorator{
+
+
+    /*
+    Holds the card which will be then decorated
+    */
+    private AbstractCard card;
+
+    /*
+    Colourblind mode.
+     */
+    public String filter;
+
+
+
+    public SGBKCard(AbstractCard card){
+        this.card = card;
+        this.filter = "SG-BK";
+    }
+
+
+    /*
+    Get the source of the front image of the card
+     */
+    public String getFrontSrc() {return card.getFrontSrc();}
+
+    /*
+    Set the source of the front image of the card
+     */
+    public void setFrontSrc(String src){this.card.setFrontSrc(src);}
+
+    /*
+    Get the source of the back image of the card
+     */
+    public String getBackSrc(){return card.getBackSrc();}
+
+    /*
+    Set the source of the back image of the card
+     */
+    public void setBackSrc(String src){this.setBackSrc(src);}
+
+
 }
