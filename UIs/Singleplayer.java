70fc8d6aa65c3da50cc40fb74e53fd4fbfbd8348
Maziar <maziarmehrafza257@gmail.com>
2022-12-05 15:05:46 -0500

    Made the single player stage and updated main menu. Co-auth: Asser, Ahmed, Manjot

--- UIs/Singleplayer.java
+++ UIs/Singleplayer.java
@@ -1,4 +1,42 @@
 package UIs;
 
-public class Singleplayer {
+import base.Board;
+import base.Game;
+import base.Player;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.StackPane;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+
+public class Singleplayer extends Stage {
+
+    StackPane y = new StackPane(); // Stores all widgets
+
+
+    Singleplayer(String col, String bot){
+        //Set title
+        this.setTitle("Single player");
+
+        //Make the back of the deck
+        ImageView deck = new ImageView();
+        deck.setImage(new Image("./Card variations/Normal/card_back.png"));
+        y.getChildren().add(deck);
+        y.setAlignment(deck, Pos.CENTER);
+
+        //Show everything
+        this.setScene(new Scene(y, 1500, 700));
+        this.show();
+    }
+
+    private Player[] getNumPlayers(){
+
+        throw new UnsupportedOperationException();
+
+    }
+
 }
+
+
